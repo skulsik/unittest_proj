3045d03cd73fdfa2e3ed4abcbed8519c3023af5f
skulsik <skulsik1@gmail.com>
2023-02-07 18:32:18 +0700

    del print

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,15 +1,34 @@
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([], -2, "test") == "test"
+def test_get_1():
+    assert arrs.get([1, 2, 3], -2) == None
+
+
+def test_get_2():
     assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([12], 0, "test") == 12
-    assert arrs.get([], 0) == None
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_get_3():
+    assert arrs.get([], 0, "test") == "test"
+
+
+def test_slice_1():
     assert arrs.my_slice([]) == []
+
+
+def test_slice_2():
+    assert arrs.my_slice([1, 2, 3], 1, 2) == [2]
+
+
+def test_slice_3():
+    assert arrs.my_slice([1, 2, 3, 4, 5], -6, 3) == [1, 2, 3]
+
+
+
+def test_slice_4():
     assert arrs.my_slice([1, 2, 3], -1) == [3]
-    assert arrs.my_slice([1, 2, 3, 4, 5], -6) == [1, 2, 3, 4, 5]
+
+
+def test_slice_5():
+    assert arrs.my_slice([1, 2, 3], -1, -5) == []
