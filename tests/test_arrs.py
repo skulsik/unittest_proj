a9030341ca646a5980941d36e3e4644f0d8d375b
skulsik <skulsik1@gmail.com>
2023-02-09 15:43:18 +0700

    add test on unitests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,34 +1,33 @@
 from utils import arrs
+import unittest
 
 
-def test_get_1():
-    assert arrs.get([1, 2, 3], -2) == None
+class Test_arrs(unittest.TestCase):
+    def test_get_1(self):
+        self.assertEqual(arrs.get([1, 2, 3], -2), None)
+
+    def test_get_2(self):
+        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+
+    def test_get_3(self):
+        self.assertEqual(arrs.get([], 0, "test"), "test")
 
 
-def test_get_2():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+    def test_slice_1(self):
+        self.assertEqual(arrs.my_slice([]), [])
 
 
-def test_get_3():
-    assert arrs.get([], 0, "test") == "test"
+    def test_slice_2(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], 1, 2), [2])
 
 
-def test_slice_1():
-    assert arrs.my_slice([]) == []
+    def test_slice_3(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], -6, 3), [1, 2, 3])
 
 
-def test_slice_2():
-    assert arrs.my_slice([1, 2, 3], 1, 2) == [2]
+    def test_slice_4(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1), [3])
 
 
-def test_slice_3():
-    assert arrs.my_slice([1, 2, 3, 4, 5], -6, 3) == [1, 2, 3]
-
-
-
-def test_slice_4():
-    assert arrs.my_slice([1, 2, 3], -1) == [3]
-
-
-def test_slice_5():
-    assert arrs.my_slice([1, 2, 3], -1, -5) == []
+    def test_slice_5(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1, -5), [])
